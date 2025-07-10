26c43c0238fa019186e165b0c361a717181ed87f
prasannasabari66 <prasannasabari66@gmail.com>
2025-07-10 15:01:53 +0530

    frontend installed tailwindcss,index.css imported tailwindcss and react-router-dom 	new file:   frontend/src/index.css 	modified:   frontend/src/main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <h1>{ENV.API_URL}</h1>
-      <p>dynamic enviroment variable</p>
+      <p className='bg-amber-200'>dynamic enviroment variable</p>
       <Card />
     </>
   )
