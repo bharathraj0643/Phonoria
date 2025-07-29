76d78a9bf68c0e12394b89503872bced35a8e4e3
prasannasabari66 <prasannasabari66@gmail.com>
2025-07-29 13:00:25 +0530

    [fixed added] : added assets 	new file:   src/assets/images/websitelogo/white-transparent-phonoria-logo.png 	modified:   src/components/Layout/Footer.jsx 	modified:   src/components/Layout/Header.jsx 	modified:   src/index.css

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import headerLogo from "/vite.svg";
+import headerLogo from "../../assets/images/websitelogo/white-transparent-phonoria-logo.png";
 
 import { TiArrowSortedDown } from "react-icons/ti";
 import { CgProfile } from "react-icons/cg";
@@ -13,16 +13,16 @@ function Header() {
           <div className="flex gap-5">
             <div className="flex shrink-0">
               <a href="" className=" ">
-                <img src={headerLogo} alt="sm:phonoria-header-logo" />
+                <img src={headerLogo} alt="sm:phonoria-header-logo" className="w-25" />
               </a>
             </div>
-            <div className="flex gap-5 basis-1/4 shrink-0 justify-end">
-              <div className="hidden sm:flex items-center text-white">
+            <div className="hidden sm:flex gap-5 basis-1/4 shrink-0 justify-end">
+              <div className=" sm:flex items-center gap-1 text-white">
                 <span>Categories</span>
                 <TiArrowSortedDown />
               </div>
             </div>
-            <div className="flex sm:hidden justify-start sm:justify-center gap-5 text-white w-full">
+            <div className="flex sm:hidden justify-center  gap-5 text-white w-full">
               <div className="flex items-center gap-1">
                 <CgProfile />
                 <span>Profile</span>
@@ -34,7 +34,7 @@ function Header() {
               </div>
             </div>
             <div className="hidden sm:flex sm:basis-1/2 justify-center w-full">
-              <input type="text" className="bg-white rounded-md w-full" />
+              <input type="text" className="bg-white rounded-md w-full px-3 focus:outline-blue-300" />
             </div>
             <div className="hidden sm:flex basis-1/4 justify-end gap-5 text-white">
               <div className="flex items-center gap-1">
@@ -54,7 +54,7 @@ function Header() {
               <TiArrowSortedDown />
             </div>
             <div className="flex sm:hidden sm:basis-1/2 justify-center w-full">
-              <input type="search" className="bg-white rounded-md w-full" />
+              <input type="search" className="bg-white rounded-md w-full focus:outline-blue-300" />
             </div>
           </div>
         </header>
