dfad0cb84e6ff6d7abc9084cc131ef3a9d8a3057
bharathraj0643 <bharathraj0643@gmail.com>
2025-07-29 10:11:39 +0530

    [ui] : header component done using flex and responsive on mobile installed react-icons 	modified:   frontend/package.json 	modified:   frontend/src/App.jsx 	modified:   frontend/src/components/Layout/Header.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    
+      <div>app</div>
     </>
   )
 }
--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import headerLogo from "../../assets/images/websitelogo/white-transparent-phonoria-logo.png"
+import React from "react";
+import headerLogo from "/vite.svg";
+
+import { TiArrowSortedDown } from "react-icons/ti";
+import { CgProfile } from "react-icons/cg";
+import { FaCartShopping } from "react-icons/fa6";
+
 function Header() {
   return (
     <>
-      <header className='container mx-auto bg-(--phonoria-color-violet) '>
-        <img src={headerLogo} alt="phonoria-header-logo" />
-
+      <header className="container mx-auto my-3 px-10 py-3 bg-(--phonoria-color-violet) max-w-7xl rounded-full">
+        <div className="flex">
+          <a href="" className="basis-1/4 shrink-0">
+            <img src={headerLogo} alt="sm:phonoria-header-logo" />
+          </a>
+          <div className="flex basis-1/2 justify-center">
+            <input type="search" className="bg-white rounded-md" />
+          </div>
+          <div className="hidden sm:flex basis-1/4 justify-end gap-5 text-white">
+            <div className="flex items-center gap-1">
+              <CgProfile />
+              <span>Profile</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <FaCartShopping />
+              <span>Cart</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex mt-3 sm:hidden justify-end gap-5 text-white">
+            <div className="flex items-center gap-1">
+              <CgProfile />
+              <span>Profile</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <FaCartShopping />
+              <span>Cart</span>
+            </div>
+          </div>
       </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
