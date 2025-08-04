d90a4479fa93d9acfcc8668dc90c0730d3b16d79
bharathraj0643 <bharathraj0643@gmail.com>
2025-08-04 12:26:59 +0530

    Squashed commit frontend 15th july to 2nd aug
    commit 449c22b [amend fixed] : fixed only the wrong branch name , not able to stop deployment status from patch and main , and thats is ok
    {to}
    commit d4c4593 cleared readme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
 
-import Card from './components/Card'
-
-import { ENV } from './constants/env'
-
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl'>{ENV.API_URL}</h1>
-      <p className='bg-amber-200'>dynamic enviroment variable</p>
-      <Card />
+      <div>app</div>
     </>
   )
 }
