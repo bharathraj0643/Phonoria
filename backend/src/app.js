fb2cb745ff00facdeefc46a5121a4b0c2eac56c3
bharathraj0643 <bharathraj0643@gmail.com>
2025-08-26 12:37:57 +0530

    [MVC] : implemented first endpoint - healthCheck 	modified:   backend/src/app.js 	new file:   backend/src/controllers/healthCheck.controllers.js 	new file:   backend/src/routes/healthCheck.routes.js 	new file:   backend/src/utils/async-handler.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
+// common middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extented: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors middleware
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
@@ -11,9 +17,8 @@ app.use(
   })
 );
 
-// common middleware
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extented: true, limit: "16kb" }));
-app.use(express.static("public"));
+// import routes
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+app.use("/api/healthcheck", healthCheckRouter);
 
 export { app };
